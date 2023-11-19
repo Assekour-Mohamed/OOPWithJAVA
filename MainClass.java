c9b059b2a7de19e35b29a6c5b9aec44f83d75c65
Assekour-Mohamed <72951139+Assekour-Mohamed@users.noreply.github.com>
2023-11-19 23:07:25 +0100

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,7 +1,10 @@
 import company.clsEmployer;
+import animal.clsAnimal;
 class clsMain {
     public static void main(String[] args) {
         clsEmployer  Emp = new clsEmployer("Mohamed","3333","programmer");
         Emp.showInfo();
+        clsAnimal animal = new clsAnimal("moooo","22");
+        animal.showAnimal();
     }
 }
